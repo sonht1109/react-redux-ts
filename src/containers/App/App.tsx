db9626f0c2f5df13452ebdbac52cb65631ad876a
sonht <sonht1109@gmail.com>
2022-08-07 11:55:23 +0700

    test lint-stage 2

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -15,9 +15,11 @@ import { appSelector } from './store';
 function App() {
   const { loading } = useSelector(appSelector);
 
+  const b = 1
+
   return (
     <LanguageProvider>
-      <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
         <GlobalSetup />
         <MapRoutes />
         {/* // extra config global */}
